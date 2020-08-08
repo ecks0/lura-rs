f6f1540bdab075807ab5a221337df0054c3044cc
Nick Zigarovich <nick@zigarovich.io>
2020-08-08 02:11:26 -0400

    add progs::systemd::AsyncService

--- src/progs/systemd.rs
+++ src/progs/systemd.rs
@@ -66,3 +66,58 @@ impl Service {
       .map(String::from)
   }
 }
+
+#[cfg(feature = "async")]
+#[derive(Debug)]
+pub struct AsyncService(String);
+
+#[cfg(feature = "async")]
+impl AsyncService {
+
+  pub fn new(name: &str) -> Self {
+    Self(name.to_owned())
+  }
+
+  pub async fn start(&self) -> Result<()> {
+    drunner().run_async("systemctl", ["start", &self.0].iter()).await?;
+    Ok(())
+  }
+
+  pub async fn stop(&self) -> Result<()> {
+    drunner().run_async("systemctl", ["stop", &self.0].iter()).await?;
+    Ok(())
+  }
+
+  pub async fn restart(&self) -> Result<()> {
+   drunner().run_async("systemctl", ["restart", &self.0].iter()).await?;
+   Ok(())
+  }
+
+  pub async fn reload(&self) -> Result<()> {
+    drunner().run_async("systemctl", ["reload", &self.0].iter()).await?;
+    Ok(())
+  }
+
+  pub async fn started(&self) -> Result<bool> {
+    Ok(drunner()
+      .run_async("systemctl", ["status", &self.0].iter()).await?
+      .code() == 0)
+  }
+
+  pub async fn stopped(&self) -> Result<bool> {
+    Ok(drunner()
+      .run_async("systemctl", ["status", &self.0].iter()).await?
+      .code() != 0)
+  }
+
+  pub async fn journal(&self, lines: usize) -> Result<String> {
+    Runner
+      ::new()
+      .capture()
+      .enforce()
+      .run_async("journalctl", ["-u", &self.0, "-n", &lines.to_string()].iter()).await?
+      .stdout()
+      .ok_or(Error::StdoutMissing)
+      .map(String::from)
+  }
+}
